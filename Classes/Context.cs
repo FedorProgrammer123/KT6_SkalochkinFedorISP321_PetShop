c4c943485c286bcdd099907754023c05f7dfbcb5
FedaSuper <63845855+FedorProgrammer123@users.noreply.github.com>
2024-10-23 18:29:24 +0300

    Add files via upload

---
+++ Classes/Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace KT6_SkalochkinFedorISP321_PetShop.Classes
+{
+    public class Context:DbContext
+    {
+        private static Context _context;
+        public static Context GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new Context();
+            }
+            return _context;
+        }
+        public Context() : base("name=TradeEntities")
+        {
+
+        }
+    }
+}
